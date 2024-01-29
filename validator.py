5d09f315da674713aec4340e9a3a9050168fb18d
Aastik <aastikdude7@gmail.com>
2024-01-29 23:39:29 +0530

    Formatting card number

--- validator.py
+++ validator.py
@@ -3,4 +3,13 @@ sum_even_digits = 0
 total = 0
 
 #Step 1
+card_number = input("Enter a credit card #: ")
+card_number = card_number.replace("-", "")
+card_number = card_number.replace(" ", "")
+card_number = card_number[::-1]
+print(card_number)
 
+#Step 2
+
+for x in card_number[::2]:
+    sum_odd_digits += x
